ddc5430a357ade03ed9591e8291c70e905a79303
CristóbalJJG <cristobal_jjg@hotmail.com>
2024-02-24 09:56:01 +0000

    Se añadieron directorios a .gitignore y mejoras de formateo

--- portfolio/src/app/components/portfolio/portfolio.component.ts
+++ portfolio/src/app/components/portfolio/portfolio.component.ts
@@ -5,28 +5,27 @@ import { Portfolio } from 'src/class/portfolio';
 @Component({
   selector: 'app-portfolio',
   template: `
-    <h1>Portfolio</h1>
     <h2 class="flex">
       <app-card *ngFor="let p of data" [portfolio]="p"></app-card>
     </h2>
   `,
   styleUrls: ['./portfolio.component.scss']
 })
-export class PortfolioComponent implements OnInit{
+export class PortfolioComponent implements OnInit {
   data: Portfolio[] = [];
 
-  async ngOnInit() {
-    await this.getPortfolio()
+  ngOnInit() {
+    this.getPortfolio()
   }
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   public getPortfolio() {
     this.http.get("../../../assets/data/portfolio.json")
       .subscribe((data: any) => {
-      let aux = data['portfolio'];
-      aux.forEach((e: Portfolio) => { this.data.push(e); })
-    });
+        let aux = data['portfolio'];
+        aux.forEach((e: Portfolio) => { this.data.push(e); })
+      });
   }
 }
 
